1128db5447c55602d9ae8062563eec77b749b0fc
Lulof Pirée <lulof.piree@zoho.com>
2022-05-14 20:19:30 +0200

    Add subplot of iron-oxygen reaction to test_step's plot

--- snakes_spn/test/test_step.py
+++ snakes_spn/test/test_step.py
@@ -28,8 +28,13 @@ File content:
 Empirical test on the 'advanced testing network' to see if the
 SPN (esp. the `PetriNet.step()`) function behaves as expected.
 """
+import matplotlib.pyplot as plt
+
 from typing import List
-from snakes_spn.test.testing_tools import H2_INIT, H2O_INIT, O2_INIT, Fe2O3_INIT, Fe_INIT, Ti_INIT, TiO2_INIT, draw_graph, draw_graph_advanced_net, setup_advanced_spn, ADVANCED_GRAPH_FILENAME
+from snakes_spn.test.testing_tools import (
+    H2_INIT, H2O_INIT, O2_INIT, Fe2O3_INIT, Fe_INIT, Ti_INIT, TiO2_INIT,
+    draw_graph, draw_graph_advanced_net, setup_advanced_spn, 
+    ADVANCED_GRAPH_FILENAME)
 
 import unittest
 from snakes.data import Substitution
@@ -41,11 +46,12 @@ import snakes.plugins
 snakes.plugins.load([spn_plugin, "gv"], "snakes.nets", "snk")
 from snk import *
 
-import matplotlib.pyplot as plt
+PLOT_FILENAME = os.path.join("snakes_spn", "test", "spn_step_sim.pdf")
+
 
 def main():
     spn = setup_advanced_spn()
-    conc = {place.name : [place.get_num_tokens()] for place in spn.place()}
+    conc = {place.name: [place.get_num_tokens()] for place in spn.place()}
     timestamps: List[float] = [0]
 
     cumul_time = 0
@@ -62,16 +68,29 @@ def main():
 
     print(f"Simulation finished after {len(timestamps)} steps")
 
-    fig, ax = plt.subplots(nrows=1, ncols=1)
-    ax.plot(timestamps, conc["oxygen"], label="$O_2$", color="red")
+    fig, axes = plt.subplots(nrows=2, ncols=1)
+
+    ax = axes[0]
+    ax.plot(timestamps, conc["oxygen"], label="$O_2$", color="blue")
     ax.plot(timestamps, conc["hydrogen"], label="$H_2$", color="green")
-    ax.plot(timestamps, conc["water"], label="$H_2O$", color="blue")
+    ax.plot(timestamps, conc["water"], label="$H_2O$", color="cyan")
     ax.set_xlabel("Time")
     ax.set_ylabel("Concentration")
     ax.set_title(r"$2H_2 + O_2 \rightarrow H_2O$")
     ax.legend()
 
+    ax = axes[1]
+    ax.plot(timestamps, conc["oxygen"], label="$O_2$", color="blue")
+    ax.plot(timestamps, conc["iron"], label="$Fe$", color="gray")
+    ax.plot(timestamps, conc["hematite"], label="$Fe_2O_3$", color="brown")
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Concentration")
+    ax.set_title(r"$4Fe + 3O_2 \rightarrow 2Fe_2O_3$")
+    ax.legend()
+
     plt.show()
+
+    # fig.savefig(PLOT_FILENAME)
 
 
 if __name__ == "__main__":
